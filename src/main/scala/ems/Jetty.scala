ec0d7a52a407cccb90b563f02be1b801ad108702
Erlend Hamnaberg <erlend@hamnaberg.net>
2016-08-31 21:47:19 +0200

    Use localhost as default

--- src/main/scala/ems/Jetty.scala
+++ src/main/scala/ems/Jetty.scala
@@ -9,7 +9,7 @@ object Jetty {
     val port = Properties.propOrElse("PORT", Properties.envOrElse("PORT", "8082")).toInt
     val contextPath = Properties.propOrElse("contextPath", Properties.envOrElse("contextPath", "/ems-video"))
     val baseURI = Properties.propOrElse("baseURI", Properties.envOrElse("baseURI", s"http://localhost:$port$contextPath"))
-    val emsRoot = URI.create(Properties.propOrElse("eventRoot", Properties.envOrElse("eventRoot", "https://javazone.no/ems/server/events/")))
+    val emsRoot = URI.create(Properties.propOrElse("eventRoot", Properties.envOrElse("eventRoot", "http://localhost:8081/server/events/")))
     val credentials = for {
       username <- Properties.propOrNone("emsUsername").orElse(Properties.envOrNone("emsUsername"))
       password <- Properties.propOrNone("emsPassword").orElse(Properties.envOrNone("emsPassword"))
