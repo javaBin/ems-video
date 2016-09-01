7f5088a5e41a0f6781235869b79188a2dbb4aaa0
Erlend Hamnaberg <erlend@hamnaberg.net>
2016-09-01 20:05:01 +0200

    EMS Base.

--- src/main/scala/ems/Jetty.scala
+++ src/main/scala/ems/Jetty.scala
@@ -4,13 +4,16 @@ import scala.util.Properties
 import unfiltered.jetty
 import java.net.URI
 
+import org.codehaus.httpcache4j.uri.URIBuilder
+
 object Jetty {
   def main(args: Array[String]): Unit = {
     val port = Properties.propOrElse("PORT", Properties.envOrElse("PORT", "8082")).toInt
     val contextPath = Properties.propOrElse("contextPath", Properties.envOrElse("contextPath", "/ems-video"))
     val baseURI = Properties.propOrElse("baseURI", Properties.envOrElse("baseURI", s"http://localhost:$port$contextPath"))
-    val emsRoot = URI.create(Properties.propOrElse("eventRoot", Properties.envOrElse("eventRoot", "http://localhost:8081/server/events/")))
-    val redirectURI = URI.create(Properties.propOrElse("redirectURI", Properties.envOrElse("redirectURI", "http://localhost:8081/server/redirect")))
+    val emsBase = URI.create(Properties.propOrElse("emsBase", Properties.envOrElse("emsBase", "http://localhost:8081/server/")))
+    val emsEvents = URIBuilder.fromURI(emsBase).addPath("events").toURI
+    val redirectURI = URIBuilder.fromURI(emsBase).addPath("redirect").toURI
     val credentials = for {
       username <- Properties.propOrNone("emsUsername").orElse(Properties.envOrNone("emsUsername"))
       password <- Properties.propOrNone("emsPassword").orElse(Properties.envOrNone("emsPassword"))
@@ -20,17 +23,17 @@ object Jetty {
       |Starting App using port $port and
       |contextPath=$contextPath
       |BaseURI is $baseURI
-      |Requesting from $emsRoot
+      |Using EMS from $emsBase
       |
       |Using credentials ${credentials.map(_.username).getOrElse("No Credentials")}
     """.stripMargin)
 
     val http = jetty.Server.http(port)
     (if (contextPath == "/" || contextPath.isEmpty) {
-      http.plan(Resources(baseURI, emsRoot, redirectURI, credentials))
+      http.plan(Resources(baseURI, emsEvents, redirectURI, credentials))
     } else {
       http.context(contextPath) {
-        _.plan(Resources(baseURI, emsRoot, redirectURI, credentials))
+        _.plan(Resources(baseURI, emsEvents, redirectURI, credentials))
       }
     }).run()
 
